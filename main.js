3c4ae2167041bad6f8ec17d5a8fb39771a80d857
Олег Рубан <rubikam@ukr.net>
2021-02-15 15:57:43 +0300

    Четвертый коммит

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ function ai() {
 }
 
 function checkEnd() {
-    if (t[0] == 'ai' && t[1] == 'ai' && t[2] == 'ai' || t[0] == 'player' && t[1] == 'player' && t[2] == 'player') return true;
-    if (t[3] == 'ai' && t[4] == 'ai' && t[5] == 'ai' || t[3] == 'player' && t[4] == 'player' && t[5] == 'player') return true;
-    if (t[6] == 'ai' && t[7] == 'ai' && t[8] == 'ai' || t[6] == 'player' && t[7] == 'player' && t[8] == 'player') return true;
-    if (t[0] == 'ai' && t[3] == 'ai' && t[6] == 'ai' || t[0] == 'player' && t[3] == 'player' && t[6] == 'player') return true;
-    if (t[1] == 'ai' && t[4] == 'ai' && t[7] == 'ai' || t[1] == 'player' && t[4] == 'player' && t[7] == 'player') return true;
-    if (t[2] == 'ai' && t[5] == 'ai' && t[8] == 'ai' || t[2] == 'player' && t[5] == 'player' && t[8] == 'player') return true;
-    if (t[0] == 'ai' && t[4] == 'ai' && t[8] == 'ai' || t[0] == 'player' && t[4] == 'player' && t[8] == 'player') return true;
-    if (t[2] == 'ai' && t[4] == 'ai' && t[6] == 'ai' || t[2] == 'player' && t[4] == 'player' && t[6] == 'player') return true;
+    if (t[0] == 'ai' && t[1] == 'ai' && t[2] == 'ai' || t[0] == 'Игрок' && t[1] == 'Игрок' && t[2] == 'Игрок') return true;
+    if (t[3] == 'ai' && t[4] == 'ai' && t[5] == 'ai' || t[3] == 'Игрок' && t[4] == 'Игрок' && t[5] == 'Игрок') return true;
+    if (t[6] == 'ai' && t[7] == 'ai' && t[8] == 'ai' || t[6] == 'Игрок' && t[7] == 'Игрок' && t[8] == 'Игрок') return true;
+    if (t[0] == 'ai' && t[3] == 'ai' && t[6] == 'ai' || t[0] == 'Игрок' && t[3] == 'Игрок' && t[6] == 'Игрок') return true;
+    if (t[1] == 'ai' && t[4] == 'ai' && t[7] == 'ai' || t[1] == 'Игрок' && t[4] == 'Игрок' && t[7] == 'Игрок') return true;
+    if (t[2] == 'ai' && t[5] == 'ai' && t[8] == 'ai' || t[2] == 'Игрок' && t[5] == 'Игрок' && t[8] == 'Игрок') return true;
+    if (t[0] == 'ai' && t[4] == 'ai' && t[8] == 'ai' || t[0] == 'Игрок' && t[4] == 'Игрок' && t[8] == 'Игрок') return true;
+    if (t[2] == 'ai' && t[4] == 'ai' && t[6] == 'ai' || t[2] == 'Игрок' && t[4] == 'Игрок' && t[6] == 'Игрок') return true;
     if (t[0] && t[1] && t[2] && t[3] && t[4] && t[5] && t[6] && t[7] && t[8]) return true;
 }
 
@@ -21,7 +21,7 @@ function move(id, role) {
     if (t[id]) return false;
     t[id] = role;
     document.getElementById(id).className = 'cell ' + role;
-    !checkEnd() ? (role == 'player') ? ai() : null : reset()
+    !checkEnd() ? (role == 'Игрок') ? ai() : null : reset()
 }
 
 function reset() {
